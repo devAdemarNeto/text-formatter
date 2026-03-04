3a3f6d5a84dce053927d47140608c500fd841616
Ademar Neto <ademarnetodev@gmail.com>
2026-03-04 18:52:43 -0300

    chore: inicializa projeto spring boot com web e thymeleaf

---
+++ src/main/java/br/com/ademar/textformatter/TextFormatterApplication.java
@@ -0,0 +1,12 @@
+package br.com.ademar.textformatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextFormatterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextFormatterApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/ademar/textformatter/TextFormatterApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.ademar.textformatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextFormatterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
