563e4d5fe83b610fe26c53fb878de9d14d6b6862
Ademar Neto <ademarnetodev@gmail.com>
2026-03-04 18:57:07 -0300

    feat: cria classe de modelo ChamadoPje com atributos do formulario

---
+++ src/main/java/br/com/ademar/textformatter/model/ChamadoPje.java
@@ -0,0 +1,62 @@
+package br.com.ademar.textformatter.model;
+
+public class ChamadoPje {
+
+    private String numeroProcesso;
+    private String nomeUsuario;
+    private String cpfCnpj;
+    private String moduloSistema;
+    private String descricaoErro;
+    private String linkPrint;
+
+    public ChamadoPje() {
+    }
+
+    public String getNumeroProcesso() {
+        return numeroProcesso;
+    }
+
+    public void setNumeroProcesso(String numeroProcesso) {
+        this.numeroProcesso = numeroProcesso;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getModuloSistema() {
+        return moduloSistema;
+    }
+
+    public void setModuloSistema(String moduloSistema) {
+        this.moduloSistema = moduloSistema;
+    }
+
+    public String getDescricaoErro() {
+        return descricaoErro;
+    }
+
+    public void setDescricaoErro(String descricaoErro) {
+        this.descricaoErro = descricaoErro;
+    }
+
+    public String getLinkPrint() {
+        return linkPrint;
+    }
+
+    public void setLinkPrint(String linkPrint) {
+        this.linkPrint = linkPrint;
+    }
+}
